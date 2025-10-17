52e9514a0ebc46985871bec2b45780405773bf2c
Boris Mirsky <boris.y.mirsky@gmail.com>
2025-10-17 21:54:32 +0300

    Заново создал папку фронтенда; нашёл наконец как использовать mui

--- frontend/frontend_folder/src/app/page.tsx
+++ frontend/frontend_folder/src/app/page.tsx
@@ -1,43 +1,27 @@
-
 "use client"
 
-import React from 'react';
 import styles from "./page.module.css";
-//import { Button } from '@material-ui/core';
-//import { Button } from '@mui/material';
-//import Button from '@material-ui/core/Button';
-import { useRouter } from 'next/navigation';
+import React from 'react';
+import { Button } from '@mui/material';
+export default function HomePage() {   
+    return (
+        <div className={styles.main}
+            style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh'
+            }}>
+            <main className={styles.main}>
+                main
+                <br />
+                <br />
+                <br />
+                <Button variant="contained" color="primary">
+                    Material-UI Button
+                </Button>
+            <br/>
+            </main>
+        </div>);
+};
 
-
-
-export default function Home() {
-    const router = useRouter();
-
-
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-              <div>
-                  <h2>Список сотрудников</h2>
-                  <br/>
-                  <h3>Fast API, SQL, React, NextJS, Typescript, antd</h3>
-                  <br />
-                  <h4>CRUD операции и другая функциональность</h4>
-                  <br/>
-                  <br />
-                  <p>
-
-                      <button
-                          onClick={() => router.push('/all_employees')}
-                          style={{ flex: 1 }}
-                      >
-                          Go
-                      </button>
-                  </p>
-        </div>
-      </main>
-      <footer className={styles.footer}>       
-      </footer>
-    </div>
-  );
-}
